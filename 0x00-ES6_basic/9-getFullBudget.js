7f7788bd63e3ce313717e34f07f37bd9c6304a45
reham-elsayed <rehamshipl445@gmail.com>
2024-07-22 18:20:57 +0200

    done

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,15 +1,12 @@
 import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: function () {
-      return `$${budget.income}`;
-    },
-    getIncomeInEuros: function () {
-      return `${budget.income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
